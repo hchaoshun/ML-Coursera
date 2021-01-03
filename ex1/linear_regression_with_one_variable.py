6849abfca7a82d2390cd86b5a408d4deb36349d3
hchaoshun <hcsxiaohan@gmail.com>
2021-01-03 11:41:47 +0800

    one variable

---
+++ ex1/linear_regression_with_one_variable.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def computeCost(X, y, theta):
+    inner = np.power(((X * theta.T) - y), 2)
+    return np.sum(inner) / (2 * len(X))
+
+# 批量梯度下降
+def gradientDescent(X, y, theta, alpha, iters):
+    temp = np.matrix(np.zeros(theta.shape))
+    parameters = int(theta.ravel().shape[1])
+    cost = np.zeros(iters)
+    
+    for i in range(iters):
+        error = (X * theta.T) - y
+        
+        for j in range(parameters):
+            term = np.multiply(error, X[:,j])
+            temp[0,j] = theta[0,j] - ((alpha / len(X)) * np.sum(term))
+            
+        theta = temp
+        cost[i] = computeCost(X, y, theta)
+        
+    return theta, cost
+
+
+
+if __name__ == '__main__':
+    path =  'ex1data1.txt'
+    data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+    # x0 = 1, 数据向量化
+    data.insert(0, 'Ones', 1)
+
+    # set X (training data) and y (target variable)
+    cols = data.shape[1]
+    X = data.iloc[:,0:cols-1]
+    y = data.iloc[:,cols-1:cols]
+
+    # 矩阵化
+    X = np.matrix(X.values)
+    y = np.matrix(y.values)
+    theta = np.matrix(np.array([0,0]))
+
+    alpha = 0.01
+    iters = 1000
+
+    g, cost = gradientDescent(X, y, theta, alpha, iters)
+    res = computeCost(X, y, g)
+
+    print("theta: ", g)
+    print("cost: ", res)
+
